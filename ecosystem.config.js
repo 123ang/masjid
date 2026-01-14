7b9ffa44800d2bcbe59a7f7d0b80452714439c37
Ang Jin Sheng <angjinsheng@gmail.com>
2026-01-14 08:34:06 +0800

    Update ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -27,7 +27,7 @@ module.exports = {
       exec_mode: 'fork',
       env: {
         NODE_ENV: 'production',
-        PORT: 3000
+        PORT: 3008
       },
       error_file: './logs/err.log',
       out_file: './logs/out.log',
